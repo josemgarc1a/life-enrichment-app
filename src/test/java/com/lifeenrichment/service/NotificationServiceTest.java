f0fb06fc8ffa7035d88033405e0448ae6467115c
Jose Garcia <josemgarc1a@icloud.com>
2026-04-21 18:30:28 -0500

    chore: remove unused ReflectionTestUtils import in NotificationServiceTest

--- src/test/java/com/lifeenrichment/service/NotificationServiceTest.java
+++ src/test/java/com/lifeenrichment/service/NotificationServiceTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
